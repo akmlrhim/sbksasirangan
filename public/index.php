728f1cd1b417bf7b431c802f4212a6ddcf29a2e1
rahimmsz <150146538+rahimmsz@users.noreply.github.com>
2026-02-14 15:02:25 +0800

    change cpanel.yml

--- public/index.php
+++ public/index.php
@@ -4,12 +4,12 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-if (file_exists($maintenance = __DIR__ . '/../sbksasirangan/storage/framework/maintenance.php')) {
+if (file_exists($maintenance = __DIR__ . '/../repositories/sbksasirangan/storage/framework/maintenance.php')) {
 	require $maintenance;
 }
 
-require __DIR__ . '/../sbksasirangan/vendor/autoload.php';
+require __DIR__ . '/../repositories/sbksasirangan/vendor/autoload.php';
 
-$app = require_once __DIR__ . '/../sbksasirangan/bootstrap/app.php';
+$app = require_once __DIR__ . '/../repositories/sbksasirangan/bootstrap/app.php';
 
 $app->handleRequest(Request::capture());
